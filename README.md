6b7edec8b6d417e3856bf0400d4e1d171abbfff8
Yuzbashiyev <tural@amazon.cz>
2019-10-08 11:26:10 +0200

    Filter instances by project. Also, stop and start instances

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Demo project to manage AWS EC2 instance snapshoots
 
 This project is a demo, and uses boto3 to manage AWS EC2 instance snapshots.
 
+# Running
+
+"python shotty/shotty.py <command> <--project=PROJECT>"
+
+*command* is list, start or stop
+*project* is optional
+
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,19 +4,62 @@ import click
 session = boto3.Session(profile_name='snapshotalyzer')
 ec2 = session.resource('ec2')
 
+def filter_instances(project):
+    instances = []
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+    return instances
 
-@click.command()
-def list_instances():
-    "List EC2 instances"
-    for i in ec2.instances.all():
+@click.group()
+def instances():
+    """Commands for instances"""
+
+
+@instances.command('list')
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    """List EC2 instances"""
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
         print(', '.join((
             i.id,
             i.instance_type,
             i.public_dns_name,
             i.state['Name'],
-            i.placement['AvailabilityZone']
+            i.placement['AvailabilityZone'],
+            tags.get('Project', '<no project>')
         )))
 
 
+@instances.command('stop')
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+def stop_instance(project):
+    """Stop EC2 instances"""
+    instances = filter_instances(project)
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+
+@instances.command('start')
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+def start_instance(project):
+    """Start EC2 instances"""
+    instances = filter_instances(project)
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+    return
+
+
 if __name__ == '__main__':
-    list_instances()
+    instances()
